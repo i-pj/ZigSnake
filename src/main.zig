b48113663cab670f205879a9fac949484584ce54
Parth Jain <42parth.jain@gmail.com>
2024-07-29 18:06:50 +0530

    game logic

--- src/main.zig
+++ src/main.zig
@@ -52,7 +52,6 @@ var allowMove: bool = false;
 var offset: rl.Vector2 = rl.Vector2Zero();
 var counterTail: i32 = 0;
 
-
 fn InitGame() void {
     framesCounter = 0;
     gameOver = false;
@@ -63,15 +62,14 @@ fn InitGame() void {
 
     // for screen position
     offset = rl.Vector2{
-       .x = @as(f32, @floatFromInt(@rem(screenWidth, SQUARE_SIZE))), // @rem calculates remainder and @as does conversion between types(https://ziglang.org/documentation/master/#as)
-       .y = @as(f32, @floatFromInt(@rem(screenHeight, SQUARE_SIZE))),
+        .x = @as(f32, @floatFromInt(@rem(screenWidth, SQUARE_SIZE))), // @rem calculates remainder and
+        .y = @as(f32, @floatFromInt(@rem(screenHeight, SQUARE_SIZE))), // @as does conversion between types(https://ziglang.org/documentation/master/#as)
     };
-
 
     // snake positions
     for (&snake) |*snake_part| {
         snake_part.* = Snake.init();
-        snake_part.position = rl.Vector2Div(rl.Vector2{.x = offset.x,.y = offset.y }, rl.Vector2{.x = 2,.y = 2 });
+        snake_part.position = rl.Vector2{ .x = offset.x / 2, .y = offset.y / 2 };
         if (snake_part != &snake[0]) {
             snake_part.color = rl.BLUE;
         }
@@ -82,7 +80,7 @@ fn InitGame() void {
         position.* = rl.Vector2Zero();
     }
 
-    fruit.size = rl.Vector2{.x = @as(f32, SQUARE_SIZE),.y = @as(f32, SQUARE_SIZE) };
+    fruit.size = rl.Vector2{ .x = @as(f32, SQUARE_SIZE), .y = @as(f32, SQUARE_SIZE) };
     fruit.color = rl.SKYBLUE;
     fruit.active = false;
 }
@@ -90,24 +88,99 @@ fn InitGame() void {
 fn UpdateGame() void {
     if (!gameOver) {
         if (rl.IsKeyPressed(rl.KEY_P)) {
-            pause =!pause;
+            pause = !pause;
         }
 
         if (!pause) {
             // Player controls
             if (rl.IsKeyPressed(rl.KEY_RIGHT) and (snake[0].speed.x == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{.x = @as(f32, SQUARE_SIZE),.y = 0 };
+                snake[0].speed = rl.Vector2{ .x = @as(f32, SQUARE_SIZE), .y = 0 };
                 allowMove = false;
             }
             if (rl.IsKeyPressed(rl.KEY_LEFT) and (snake[0].speed.x == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{.x = @as(f32, -SQUARE_SIZE),.y = 0 };
+                snake[0].speed = rl.Vector2{ .x = @as(f32, -SQUARE_SIZE), .y = 0 };
                 allowMove = false;
             }
             if (rl.IsKeyPressed(rl.KEY_UP) and (snake[0].speed.y == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{.x = 0,.y = @as(f32, -SQUARE_SIZE) };
+                snake[0].speed = rl.Vector2{ .x = 0, .y = @as(f32, -SQUARE_SIZE) };
                 allowMove = false;
             }
             if (rl.IsKeyPressed(rl.KEY_DOWN) and (snake[0].speed.y == 0) and allowMove) {
-                snake[0].speed = rl.Vector2{.x = 0,.y = @as(f32, SQUARE_SIZE) };
+                snake[0].speed = rl.Vector2{ .x = 0, .y = @as(f32, SQUARE_SIZE) };
                 allowMove = false;
             }
+            // Snake movement
+            var j: usize = 0;
+            while (j < @as(usize, @intCast(counterTail))) : (j += 1) {
+                snakePosition[j] = snake[j].position;
+            }
+
+            if (@rem(framesCounter, 5) == 0) {
+                j = 0;
+                while (j < @as(usize, @intCast(counterTail))) : (j += 1) {
+                    if (j == 0) {
+                        snake[0].position = rl.Vector2Add(snake[0].position, snake[0].speed);
+                        allowMove = true;
+                    } else {
+                        snake[j].position = snakePosition[j - 1];
+                    }
+                }
+            }
+
+            // Wall behaviour
+            if ((snake[0].position.x > (@as(f32, @floatFromInt(screenWidth)) - offset.x)) or
+                (snake[0].position.y > (@as(f32, @floatFromInt(screenHeight)) - offset.y)) or
+                (snake[0].position.x < 0) or (snake[0].position.y < 0))
+            {
+                gameOver = true;
+            }
+
+            // Collision with self check!
+            j = 1;
+            while (j < @as(usize, @intCast(counterTail))) : (j += 1) {
+                if (snake[0].position.x == snake[j].position.x and snake[0].position.y == snake[j].position.y) {
+                    gameOver = true;
+                    break;
+                }
+            }
+
+            // Fruit position calculation
+            if (!fruit.active) {
+                fruit.active = true;
+                fruit.position = rl.Vector2{
+                    .x = @as(f32, @floatFromInt(rl.GetRandomValue(0, @divTrunc(screenWidth, SQUARE_SIZE) - 1) * SQUARE_SIZE)) + (offset.x / 2),
+                    .y = @as(f32, @floatFromInt(rl.GetRandomValue(0, @divTrunc(screenHeight, SQUARE_SIZE) - 1) * SQUARE_SIZE)) + (offset.y / 2),
+                };
+
+                //check to make sure the fruit is not on the snake
+                j = 0;
+                while (j < @as(usize, @intCast(counterTail))) : (j += 1) {
+                    while (fruit.position.x == snake[j].position.x and fruit.position.y == snake[j].position.y) {
+                        fruit.position = rl.Vector2{
+                            .x = @as(f32, @floatFromInt(rl.GetRandomValue(0, @divTrunc(screenWidth, SQUARE_SIZE) - 1) * SQUARE_SIZE)) + (offset.x / 2),
+                            .y = @as(f32, @floatFromInt(rl.GetRandomValue(0, @divTrunc(screenHeight, SQUARE_SIZE) - 1) * SQUARE_SIZE)) + (offset.y / 2),
+                        };
+                        j = 0;
+                    }
+                }
+            }
+
+            // Collision with fruit
+            if (rl.CheckCollisionRecs(
+                rl.Rectangle{ .x = snake[0].position.x, .y = snake[0].position.y, .width = snake[0].size.x, .height = snake[0].size.y },
+                rl.Rectangle{ .x = fruit.position.x, .y = fruit.position.y, .width = fruit.size.x, .height = fruit.size.y },
+            )) {
+                snake[@intCast(counterTail)].position = snakePosition[@intCast(counterTail - 1)];
+                counterTail += 1;
+                fruit.active = false;
+            }
+
+            framesCounter += 1;
+        }
+    } else {
+        if (rl.IsKeyPressed(rl.KEY_ENTER)) {
+            InitGame();
+            gameOver = false;
+        }
+    }
+}
